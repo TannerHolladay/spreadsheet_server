14d3cedb12f7a6f42ceaa1a29e9b9d0d78837030
jimmyglasscock <jamesglasscock99@gmail.com>
2021-04-10 16:21:49 -0600

    began work on client class

---
+++ client.h
@@ -0,0 +1,14 @@
+#ifndef CLIENT
+#define CLIENT
+class client{
+	public:
+		//constructor to setup socket
+		//destructor
+		//getCurrentSpreadsheet
+		//sendData
+	private:
+		//socket - be initialized by constructor
+		//currentSpreadsheet - string containing the name of the spreadsheet the client is connected to
+		char* currentSpreadsheet;
+};
+#endif
