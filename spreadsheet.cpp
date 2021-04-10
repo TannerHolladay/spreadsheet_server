c1b3fec81e86831a57d43e642c038a69a3f41dd2
jimmyglasscock <jamesglasscock99@gmail.com>
2021-04-10 16:56:41 -0600

    updated spreadsheet.h

---
+++ spreadsheet.cpp
@@ -0,0 +1,19 @@
+void spreadsheet::spreadsheet(){
+
+}
+
+void spreadsheet::~spreadsheet(){
+
+}
+
+void spreadsheet::updateCell(char* cellName, char* contents){
+
+}
+
+void spreadsheet::undo(){
+
+}
+
+void spreadsheet::revert(char* cellName){
+	
+}
--- spreadsheet.h
+++ spreadsheet.h
@@ -1,16 +1,21 @@
 #ifndef SHEET
 #define SHEET
+
+#include <map>
+
+typedef std::pair<char*, char*> cellState;
+
 class spreadsheet{
 	public:
-		spreadsheet();
-		~spreadsheet(); //destructor
-		//updateCell
-		//updateSelectedCell
-		//undo
-		//revert
+		void spreadsheet();
+		void ~spreadsheet(); //destructor
+		void updateCell(char* cellName, char* contents);
+		void undo();
+		void revert(char* cellName);
 	private:
-		//cells - hashset
-		//name - name of the spreadsheet
+		std::map<char*, cell> cells;
+		char* spreadsheetName;
 
+		std::stack<cellState> undoStack;
 };
 #endif
