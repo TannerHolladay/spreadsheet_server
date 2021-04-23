c6cb97d58b6a18fd9ad9b11d254a8e3bb5590bb3
tschnide <travis.schnider@gmail.com>
2021-04-23 00:23:01 +0000

    Create working tests. Add documentation.

--- tests.cc
+++ tests.cc
@@ -1,10 +1,37 @@
+/* For more information check out the primer:  		https://google.github.io/googletest/primer.html
+ * For example tests check out the github examples: 	https://github.com/google/googletest/tree/master/googletest/samples
+ * 
+ * Each of the tests can have multiple assertions but they typically cover a single feature.
+ * The test output will look best if the class and the feature are passed into TEST().
+ *
+ * To run the tests go to the program root directory and run:
+ * 
+ * cmake -S . -B build
+ * cmake --build build
+ * cd build && ctest
+ *
+ */
+
 #include <gtest/gtest.h>
+#include "cell.h"
+#include "spreadsheet.h"
+#include "client.h"
 
-// Demonstrate some basic assertions.
- TEST(Tests, BasicAssertions) {
-     // Expect two strings not to be equal.
-     EXPECT_STRNE("hello", "world");
+TEST(Cell, EqualityOperator) {
+    cell cell1("1+1", "A1");
+    cell cell2("1+1", "A1");
+    cell cell3("1+1", "A2");
 
-     // Expect equality.
-     EXPECT_EQ(7 * 6, 42);
+    ASSERT_TRUE(cell1 == cell2);
+    ASSERT_FALSE(cell1 == cell3);
 }
+
+TEST(Spreadsheet, updateCell) {
+    ASSERT_EQ("spreadsheet", "spreadsheet");
+}
+
+TEST(Client, sendMessage) {
+    ASSERT_GE(2, 1);
+}
+
+
