e508192fdef80d6d27dab5326a56019daf0e7718
TannerHolladay <tannerholladay6@gmail.com>
2021-04-23 12:34:37 -0600

    Cleaned up server code and added documentation

--- client.cpp
+++ client.cpp
@@ -3,96 +3,138 @@
 #include "client.h"
 #include "spreadsheet.h"
 
-client::client(boost::asio::io_context& io_context) : socket(io_context) {
-
-}
+client::client(tcp::socket socket) : socket(std::move(socket)) {}
 
 client::~client() {
-    //socket.close??
-    std::cout << "deleted" << std::endl;
+    socket.close();
 }
 
-std::string client::getCurrentSpreadsheet() {
-    return this->currentSpreadsheet;
-}
+// The handshake to get the client's name and spreadsheet to connect the user to the server
+void client::doHandshake() {
+    auto self(shared_from_this());
+    socket.async_read_some(
+            boost::asio::buffer(data, max_length),
+            [this, self](const boost::system::error_code& error, std::size_t bytes_transferred) {
+                if (!error) {
+                    // If no information is received then repeat
+                    if (bytes_transferred == 0) {
+                        doHandshake();
+                        return;
+                    }
 
-void client::sendMessage(std::string message) {
-    boost::asio::async_write(
-            socket,
-            boost::asio::buffer(message),
-            boost::bind(
-                    &client::handleWrite,
-                    shared_from_this(),
-                    boost::asio::placeholders::error,
-                    boost::asio::placeholders::bytes_transferred
-            )
+                    // Adds a null character at the end to indicate the end of the data received.
+                    data[bytes_transferred] = '\0';
+
+                    // Keep receiving data until a new line character is reached
+                    buffer += data;
+                    if (data[bytes_transferred - 1] != '\n') {
+                        doHandshake();
+                        return;
+                    }
+
+                    // Checks to see if the buffer is empty or only whitespace
+                    if (!buffer.empty() &&
+                        !std::all_of(buffer.begin(), buffer.end(), [](char c) { return std::isspace(c); })) {
+
+                        // If the client's name isn't set, then it applies the buffer to the username. Otherwise, it joins the user to a spreadsheet.
+                        if (this->userName.empty()) {
+                            std::cout << "User joined: " << buffer << std::endl;
+                            this->userName = buffer;
+                            // After the username is received, the server sends the spreadsheets
+                            if (!spreadsheet::spreadsheets.empty()) {
+                                std::string message;
+                                std::map<std::string, spreadsheet*>::iterator it;
+                                for (it = spreadsheet::spreadsheets.begin();
+                                     it != spreadsheet::spreadsheets.end(); it++) {
+                                    message += it->first + "\n";
+                                }
+                                sendMessage(message + "\n");
+                            } else {
+                                // Sends two new line characters to indicate there are no created spreadsheets
+                                sendMessage("\n\n");
+                            }
+                        } else {
+                            // Creates a new spreadsheet if it does not exist
+                            if (spreadsheet::spreadsheets.count(buffer) == 0) {
+                                spreadsheet::spreadsheets[buffer] = new spreadsheet(buffer);
+                                std::cout << "Created new spreadsheet: " << buffer << std::endl;
+                            }
+                            // Join the spreadsheet
+                            currentSpreadsheet = spreadsheet::spreadsheets[buffer];
+                            currentSpreadsheet->join(shared_from_this());
+                            return;
+                        }
+                    }
+
+                    buffer = ""; // Resets the buffer to be able to add more information
+                    doHandshake();
+                } else {
+                    // Closes the socket connection if any errors happened when connecting
+                    closeSocket(error);
+                }
+            }
     );
 }
 
+// Sends a message to the client
+void client::sendMessage(const std::string& message) {
+    auto self(shared_from_this());
+    boost::asio::async_write(
+            socket,
+            boost::asio::buffer(message.c_str(), message.size()),
+            [this, self](boost::system::error_code error, std::size_t) {
+                if (error) {
+                    closeSocket(error);
+                }
+            }
+    );
+}
+
+// Event loop to process information into the spreadsheet from the client
+void client::doRead() {
+    auto self(shared_from_this());
+    socket.async_read_some(
+            boost::asio::buffer(data, max_length),
+            [this, self](const boost::system::error_code& error, std::size_t bytes_transferred) {
+                if (!error) {
+                    // If no information is received then repeat
+                    if (bytes_transferred == 0) {
+                        doHandshake();
+                        return;
+                    }
+
+                    // Adds a null character at the end to indicate the end of the data received.
+                    data[bytes_transferred] = '\0';
+
+                    // Keep receiving data until a new line character is reached
+                    buffer += data;
+
+                    // TODO: Handle event loop information here
+                    // Maybe combine this method with doHandshake some way? Like an if statement or something else?
+
+                    buffer = ""; // Resets the buffer to be able to add more information
+                    doHandshake();
+                } else {
+                    // Closes the socket connection if any errors happened when connecting
+                    closeSocket(error);
+                }
+            }
+    );
+}
+
+// Get the spreadsheet that the client is connected to
+spreadsheet* client::getCurrentSpreadsheet() {
+    return this->currentSpreadsheet;
+}
+
+// Set the cell this client is currently selecting
 void client::setSelectedCell(std::string cellName) {
     this->currentSelectedCell = cellName;
 }
 
-void client::handleWrite(const boost::system::error_code&, size_t) {
-
-}
-
-void client::readMessage() {
-    socket.async_read_some(
-            boost::asio::buffer(data, max_length),
-            boost::bind(
-                    &client::handleMessage,
-                    shared_from_this(),
-                    boost::asio::placeholders::error,
-                    boost::asio::placeholders::bytes_transferred
-            )
-    );
-}
-
-void client::handleMessage(const boost::system::error_code& error, std::size_t bytes_transferred) {
-    if (!error) {
-        if (bytes_transferred == 0) {
-            readMessage();
-            return;
-        }
-
-        data[bytes_transferred] = '\0';
-
-        buffer += data;
-
-        if (data[bytes_transferred-1] != '\n') {
-            readMessage();
-            return;
-        }
-
-        std::cout << buffer << std::endl;
-
-        if (userName == "") {
-            userName = buffer;
-            if (spreadsheet::spreadsheets.size() != 0){
-                std::string message = "";
-                for (auto key: spreadsheet::spreadsheets){
-                    message += key.first+"\n";
-                }
-                sendMessage(message + "\n");
-            } else {
-                sendMessage("Create new Spreadsheet\n");
-            }
-        } else {
-            if (spreadsheet::spreadsheets.count(buffer) == 0) {
-                spreadsheet::spreadsheets[buffer] = new spreadsheet(buffer);
-                std::cout << "Created new spreadsheet" << std::endl;
-            }
-            spreadsheet::spreadsheets[buffer]->join(shared_from_this());
-            return;
-        }
-
-        buffer = "";
-
-        readMessage();
-    }
-}
-
-void client::connect() {
-    readMessage();
+// Closes the socket
+void client::closeSocket(boost::system::error_code error) {
+    currentSpreadsheet->disconnect(shared_from_this());
+    socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both, error);
+    socket.close();
 }
--- client.h
+++ client.h
@@ -2,49 +2,48 @@
 #define CLIENT
 
 #include <boost/asio.hpp>
-#include <boost/thread.hpp>
-#include <sys/socket.h>
+#include <memory>
 
 using boost::asio::ip::tcp;
 
-class client : public boost::enable_shared_from_this<client> {
-public:
-    enum { max_length = 5 };
-    typedef boost::shared_ptr<client> pointer;
+class spreadsheet;
 
-    static pointer create(boost::asio::io_context& io_context) {
-        return pointer(new client(io_context));
-    }
+class client : public std::enable_shared_from_this<client> {
+public:
+    typedef std::shared_ptr<client> pointer;
+
+    client(tcp::socket socket);
 
     ~client();
 
-    std::string getCurrentSpreadsheet();
+    void doHandshake();
 
-    void sendMessage(std::string data);
+    void sendMessage(const std::string& data);
 
-    void handleMessage(const boost::system::error_code& error, std::size_t bytes_transferred);
+    void doRead();
+
+    void closeSocket(boost::system::error_code error);
+
+    spreadsheet* getCurrentSpreadsheet();
 
     void setSelectedCell(std::string cellName);
 
-    void connect();
-
-    //socket - be initialized by constructor
-    tcp::socket socket;
-    char data[max_length];
-    std::string buffer;
 private:
-    client(boost::asio::io_context& io_context);
+    std::string userName;
 
     //currentSpreadsheet - string containing the name of the spreadsheet the client is connected to
-    std::string currentSpreadsheet;
+    spreadsheet* currentSpreadsheet;
+
     //this is the cell the client has selected
     std::string currentSelectedCell;
 
-    std::string userName;
-
-    void handleWrite(const boost::system::error_code&, size_t);
-
-    void readMessage();
+    //socket - be initialized by constructor
+    tcp::socket socket;
+    enum {
+        max_length = 1024
+    };
+    char data[max_length];
+    std::string buffer;
 };
 
 #endif
