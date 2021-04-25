04df6dce7b32b38f860f7a7ea2d86aafba9c0735
tschnide <travis.schnider@gmail.com>
2021-04-25 04:37:59 +0000

    create method to deserialize (handleRawRequest)

--- client.h
+++ client.h
@@ -44,8 +44,8 @@ private:
     };
     char data[max_length];
     std::string buffer;
-
-    struct * deserialize(const std::string JSONMessage);
+    
+    void handleRawRequest(const std::string JSONMessage);
 };
 
 #endif
