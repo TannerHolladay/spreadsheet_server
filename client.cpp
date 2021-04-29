15fff69732ca0be042a48f852c4e58ae03b39759
TannerHolladay <tannerholladay6@gmail.com>
2021-04-29 17:17:33 -0600

    Reset buffer after spreadsheet is assigned

--- client.cpp
+++ client.cpp
@@ -77,6 +77,7 @@ void client::doRead() {
                             // Join the spreadsheet
                             currentSpreadsheet = spreadsheet::spreadsheets[buffer];
                             currentSpreadsheet->join(shared_from_this());
+                            buffer = "";
                             return;
                         } else {
                             // Handles incoming json requests
