3fc732e07e09f366c0234b9e06144a680154ead4
Noah Carlson <noahrk14@gmail.com>
2021-04-28 21:33:11 -0600

    Added Tests

--- Test.cpp
+++ Test.cpp
@@ -22,52 +22,77 @@ void TestName_Condition_Result() {
 }
 */
 
-void Cell_WhenEmptyRevert_ReturnsNoContets();
-void Cell_AfterUpdate_ReturnsNoContets();
+void Cell_WhenEmptyRevert_ReturnsNoContents();
+void Cell_AfterUpdate_ReturnsNoContents();
 void Cell_WithContents_CanRevert();
 void Cell_WithoutContents_CanRevert();
 void Cell_AfterUpdate_CanRevert();
+void Cell_Get_Contents1();
+void Cell_Update_Contents1();
+void Cell_Equality_Operator1();
 
 int main()
 {
-    Cell_WhenEmptyRevert_ReturnsNoContets();
-    Cell_AfterUpdate_ReturnsNoContets();
+    std::cout << "CELL: Revert Tests:" << std::endl << std::endl;
+    Cell_WhenEmptyRevert_ReturnsNoContents();
+    Cell_AfterUpdate_ReturnsNoContents();
+    std::cout << std::endl;
+
+    std::cout << "CELL: CanRevert Tests:" << std::endl << std::endl;
     Cell_WithContents_CanRevert();
     Cell_WithoutContents_CanRevert();
     Cell_AfterUpdate_CanRevert();
+    std::cout << std::endl;
+
+    std::cout << "CELL: Get Cell Contents Tests:" << std::endl << std::endl;
+    Cell_Get_Contents1();
+    std::cout << std::endl;
+
+    std::cout << "CELL: Update Contents Tests:" << std::endl << std::endl;
+    Cell_Update_Contents1();
+    std::cout << std::endl;
+
+    std::cout << "CELL: Equality operator Tests:" << std::endl << std::endl;
+    Cell_Equality_Operator1();
+    std::cout << std::endl;
+
 }
+
 
 /*******************************************************
  *
- * Spreadsheet tests
+ * Cell tests
  *
  *******************************************************/
 
-void Cell_WhenEmptyRevert_ReturnsNoContets() {
+
+void Cell_WhenEmptyRevert_ReturnsNoContents() {
 
     cell cell1("1","A6");
-    cell1.revert();
-	std::string result = cell1.getContents();
 
-	if(result == "") {
-		std::cout << "---Pass--- Cell_WhenEmptyRevert_ReturnsNoContets" << std::endl;
+    cell1.revert();
+
+	bool isPassing = cell1.getContents() == "";
+
+	if(isPassing) {
+		std::cout << "---Pass--- Cell_WhenEmptyRevert_ReturnsNoContents" << std::endl;
 	}
 	else {
-		std::cout << "---Fail--- Cell_WhenEmptyRevert_ReturnsNoContets" << std::endl;
+		std::cout << "---Fail--- Cell_WhenEmptyRevert_ReturnsNoContents" << std::endl;
 	}
 }
 
-void Cell_AfterUpdate_ReturnsNoContets() {
+void Cell_AfterUpdate_ReturnsNoContents() {
 
     cell cell6("1","A6");
     cell6.revert();
 	std::string result = cell6.getContents();
 
 	if(result == "") {
-		std::cout << "---Pass--- Cell_AfterUpdate_ReturnsNoContets" << std::endl;
+		std::cout << "---Pass--- Cell_AfterUpdate_ReturnsNoContents" << std::endl;
 	}
 	else {
-		std::cout << "---Fail--- Cell_AfterUpdate_ReturnsNoContets" << std::endl;
+		std::cout << "---Fail--- Cell_AfterUpdate_ReturnsNoContents" << std::endl;
 	}
 }
 
@@ -97,15 +122,64 @@ void Cell_WithoutContents_CanRevert() {
 
 void Cell_AfterUpdate_CanRevert() {
 
-    cell cell1("","A3");
+    cell cell1("", "A3");
     cell1.updateContents("2");
 
-	if(cell1.canRevert()) {
-		std::cout << "---Pass--- Cell_AfterUpdate_CanRevert" << std::endl;
-	}
-	else {
-		std::cout << "---Fail--- Cell_AfterUpdate_CanRevert" << std::endl;
-	}
+    if (cell1.canRevert()) {
+        std::cout << "---Pass--- Cell_AfterUpdate_CanRevert" << std::endl;
+    } else {
+        std::cout << "---Fail--- Cell_AfterUpdate_CanRevert" << std::endl;
+    }
 }
 
+
+////////////CELL: GetContents/////////////////
+void Cell_Get_Contents1() {
+    cell cell1("1", "A1");
+    bool isPassing = cell1.getContents() == "1";
+
+    if(isPassing)
+    {
+        std::cout << "---Pass--- Cell_Get_Contents1" << std::endl;
+    }
+    else
+    {
+        std::cout << "---Fail--- Cell_Get_Contents1" << std::endl;
+    }
+}
+
+////////////CELL: UpdateContents/////////////////
+
+void Cell_Update_Contents1() {
+    cell cell2("1", "A2");
+    cell2.updateContents("2");
+    bool isPassing = cell2.getContents() == "2";
+
+    if(isPassing)
+    {
+        std::cout << "---Pass--- Cell_Update_Contents1" << std::endl;
+    }
+    else
+    {
+        std::cout << "---Fail--- Cell_Update_Contents1" << std::endl;
+    }
+}
+
+////////////CELL: Equality operator/////////////////
+
+
+void Cell_Equality_Operator1() {
+    cell cell1("1+1", "A1");
+    cell cell2("1+1", "A1");
+    bool isPassing = cell1 == cell2;
+
+    if(isPassing)
+    {
+        std::cout << "---Pass--- Cell_Equality_Operator1" << std::endl;
+    }
+    else
+    {
+        std::cout << "---Fail--- Cell_Equality_Operator1" << std::endl;
+    }
+}
 
