70611ec329304ecd6711e7522efe3bf4aa54cf42
TannerHolladay <tannerholladay6@gmail.com>
2021-04-29 00:57:46 -0600

    Removed unwanted files/folders

--- build/tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/spreadsheet_server/build/tests[1]_tests.cmake")
-  include("/spreadsheet_server/build/tests[1]_tests.cmake")
-else()
-  add_test(tests_NOT_BUILT tests_NOT_BUILT)
-endif()
